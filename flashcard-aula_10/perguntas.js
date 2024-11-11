ce2603de64aaddb6064ef66efc6ba8329b47380e
mateusgoe <164902042+mateusgoe@users.noreply.github.com>
2024-11-11 12:40:45 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Quem sou eu?',
+    '',
+    'Meu nome é Mateus Goes, tenho 16 anos, tenho 2 irmãos epratico esportes.'
+)
+
+criaCartao(
+    'Minha amília?',
+    '',
+    'Eu moro com meu pai, minha mãe e meu irmãozinho. Meu outro irmão mora sozinho.'
+)
+
+criaCartao(
+    'Onde moro?',
+    '',
+    'moro em Pinhão no predio do Itaú nas seguintes cordenas: -25.69610477738051, -51.65659231693873.'
+)
+
+criaCartao(
+    'Onde estudo?',
+    '',
+    'Estudo no Colegio Estadual Professor Mário Evaldo Morski. Fui fundado em 1987.'
+)
+
+
+criaCartao(
+    'Minha turma?',
+    '',
+    'Temos 33 estudantes, tem uma aluna que não vai para aula pois esta grávida.'
+)
+
+criaCartao(
+    'Faculdade?',
+    '',
+    'Ainda não sei ao certo o que quero fazer mas talves tente medicina.'
+)
+
+criaCartao(
+    'Minha cidade?',
+    '',
+    'Moro em Pinhão uma pequena cidade no interior do Paraná, eu gosto dessa cidade.'
+)
+
+criaCartao(
+    'Meu sonho?',
+    '',
+    'Quero conseguir dar uma boa condição para minha família e ser feliz.'
